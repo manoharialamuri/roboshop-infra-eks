a152419dc02e8b955b09b05371c7cb1717a9fa04
manu <example@gmail.com>
2026-04-21 16:31:32 +0530

    sg

--- 20-sg-rules/data.tf
+++ 20-sg-rules/data.tf
@@ -27,7 +27,7 @@ data "aws_ssm_parameter" "rabbitmq_sg_id" {
 }
 
 data "aws_ssm_parameter" "ingress_alb_sg_id" {
-  name = "/${var.project}/${var.environment}/frontend_alb_sg_id"
+  name = "/${var.project}/${var.environment}/ingress_alb_sg_id"
 }
 
 data "aws_ssm_parameter" "eks_control_plane_sg_id" {
