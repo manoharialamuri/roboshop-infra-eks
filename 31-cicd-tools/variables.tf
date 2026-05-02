6ad06c5db34a7c6f4fd7cd6ae120a8c8b6d77ec5
manu <example@gmail.com>
2026-05-02 11:21:33 +0530

    cicd

---
+++ 31-cicd-tools/variables.tf
@@ -0,0 +1,19 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z0996154EJGGPGBA9M2V"
+}
+
+variable "domain_name" {
+    default = "daws88s.store"
+}
+
+variable "sonar" {
+  default = "false"
+}
