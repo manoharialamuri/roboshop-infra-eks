586cfc687c955582f195cc0c0eb71ee61eda0d27
manu <example@gmail.com>
2026-05-02 11:06:31 +0530

    sg

--- 10-sg/variables.tf
+++ 10-sg/variables.tf
@@ -23,7 +23,9 @@ variable "sg_names" {
     "bastion",
     #openvpn
     "openvpn",
-    "eks_control_plane", "eks_node"
+    "eks_control_plane", "eks_node",
+    "jenkins", "jenkins-agent",
+    "sonar"
   ]
 }
 
