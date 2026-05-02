6b1ae06c5d1dde4b9b57b8d69b59bd0e5d5d6608
manu <example@gmail.com>
2026-05-02 11:42:19 +0530

    cicd

--- 20-sg-rules/main.tf
+++ 20-sg-rules/main.tf
@@ -168,7 +168,7 @@ resource "aws_security_group_rule" "jenkins_ssh" {
   to_port           = 22
   protocol          = "tcp" # all traffic
   # VPC CIDR
-  cidr_blocks = ["0.0.0.0/16"]
+  cidr_blocks = ["0.0.0.0/0"]
   security_group_id = local.jenkins_sg_id
 }
 
